df2fac8edd92e9b0707e25128d1e34925602446e
Akshay Sasikumar <v.akshay.s@gmail.com>
2021-11-12 16:38:09 -0500

    Add support for running on CUDA GPU if available

--- model/model.py
+++ model/model.py
@@ -11,13 +11,20 @@ from PIL import Image
 from torch.nn import Linear
 from torch.utils.data import Dataset, DataLoader
 from torchvision.transforms import transforms
+import torchvision
 from tqdm import tqdm
 from transformers import DistilBertModel, DistilBertTokenizer
 from sklearn.model_selection import train_test_split
 
-
+device = torch.device('cpu')
 # set computation device
-device = ('cuda:0' if torch.cuda.is_available() else 'cpu')
+if(torch.cuda.is_available()):
+    # Get the GPU device name.
+    device_name = torch.cuda.get_device_name()
+    n_gpu = torch.cuda.device_count()
+    print(f"Found device: {device_name}, n_gpu: {n_gpu}")
+    device = torch.device("cuda")
+print(device)
 
 
 class ICErrorDataSet(Dataset):
@@ -85,9 +92,11 @@ class CaptionErrorDetectorBase(nn.Module):
 
         # Bert embedding for textual part.
         self.bert_model = DistilBertModel.from_pretrained('distilbert-base-uncased')
+        if torch.cuda.is_available():
+          self.bert_model.cuda()
 
         # ResNet embedding for image part. We use the last FCa layer's output.
-        self.resnet_model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', pretrained=True)
+        self.resnet_model = torchvision.models.resnet50(pretrained=True)
         # print(self.resnet_model)
 
         self.linear1 = Linear(768 + 1000, 100)
@@ -136,111 +145,165 @@ def load_train_val_data(num_examples=100, batch_size=20) -> (DataLoader, DataLoa
     return train_dataloader, val_dataloader
 
 
-def train_model():
-    train_loader, val_loader = load_train_val_data(num_examples=10, batch_size=10)
+def train_model(num_examples=30, batch_size=10):
+    if(torch.cuda.is_available()):
+        torch.cuda.empty_cache()
+    train_loader, val_loader = load_train_val_data(num_examples=num_examples, batch_size=batch_size)
     print('Data loaded.')
 
     my_model = CaptionErrorDetectorBase()
+    my_model.to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.AdamW(my_model.parameters())
-
-    max_epochs = 50
+    optimizer = optim.AdamW(my_model.parameters(),
+        lr = 1e-4,
+        eps = 1e-5
+    )
+    max_epochs = 30
     for epoch in tqdm(range(max_epochs), total=max_epochs):
         print(f'Epoch: {epoch + 1}')
 
+        train_loss = 0
+        train_correct = 0
+
         # Training the model.
         my_model.train()
-        for local_batch, local_labels in train_loader:
-            # print(local_batch, local_labels)
+        for i, data in enumerate(train_loader):
+            local_batch, local_labels = data[0], data[1]
             optimizer.zero_grad()
 
-            outputs = my_model(local_batch)
-            loss = criterion(outputs, local_labels)
+            text = local_batch['text']
+            image = local_batch['image']
+            input_ids = text['input_ids']
+            attention_mask = text['attention_mask']
+            device_input = {
+                'image': image.to(device),
+                'text' : {
+                    'input_ids': input_ids.to(device),
+                    'attention_mask' : attention_mask.to(device)
+                }
+            }
+            device_local_labels = local_labels.to(device)
+
+            outputs = my_model(device_input)
+            loss = criterion(outputs, device_local_labels)
             loss.backward()
             optimizer.step()
 
-            print('train batch loss={:.3g}'.format(loss))
             _, preds = torch.max(outputs.data, 1)
-            print('train batch accuracy={:.3g}'.format((preds == local_labels).sum().item() / local_labels.shape[0]))
 
-        # Perform validation
-        my_model.eval()
-        val_running_loss = 0.0
-        val_running_correct = 0
-        with torch.no_grad():
-            # for i, data in tqdm(enumerate(val_loader), total=int(len(val_loader.dataset) / val_loader.batch_size)):
-            for i, data in enumerate(val_loader):
-                local_batch, local_labels = data[0], data[1]
-                # local_batch['text'] = local_batch['text'].to(device)
-                # local_batch['image'] = local_batch['image'].to(device)
-                outputs = my_model(local_batch)
-                loss = criterion(outputs, local_labels)
+            train_loss += loss.item()
+            train_correct += (preds == device_local_labels).sum().item()
 
-                val_running_loss += loss.item()
-                _, preds = torch.max(outputs.data, 1)
-                val_running_correct += (preds == local_labels).sum().item()
+            # print('train batch loss={:.3g}'.format(loss))
+            # print('train batch accuracy={:.3g}'.format((preds.to('cpu') == local_labels).sum().item() / local_labels.shape[0]))
 
-            val_loss = val_running_loss / len(val_loader.dataset)
-            val_accuracy = 100. * val_running_correct / len(val_loader.dataset)
-            print(f'Epoch: {epoch + 1}, Val Loss: {val_loss:.4f}, Val Acc: {val_accuracy:.2f}')
-
+            # print('train batch loss={:.3g}'.format(loss))
+            if(i%10 == 0):
+                validate(val_loader, my_model, criterion)
+        print()
+        train_accuracy = 100 * train_correct/len(train_loader.dataset)
+        print(f'Epoch: {epoch + 1}, \nTrain Loss: {train_loss:.4f}, Train Acc: {train_accuracy}')
+#         wandb.log({'train_accuracy': train_accuracy})
+#         wandb.log({'train_loss': train_loss})
+        validate(val_loader, my_model, criterion)
+        print()
     print('training done.')
 
 
-def test_load_data():
-    X_orig = [
-        ("person in read riding a motorcycle", "http://farm9.staticflickr.com/8186/8119368305_4e622c8349_z.jpg", "motorcycle.jpg"),
-        ("lady cutting cheese with reversed knife", "http://farm1.staticflickr.com/1/127244861_ab0c0381e7_z.jpg", "cheese.jpg"),
-        ("girl touching a buffalo", "http://farm3.staticflickr.com/2169/2118578392_1193aa04a0_z.jpg", "boy.jpg")
-    ]
-    Y_orig = ['true', 'true', 'false']
+def validate(val_loader, my_model, loss_func):
+    # Perform validation
+    my_model.eval()
+    val_running_loss = 0.0
+    val_running_correct = 0
+    with torch.no_grad():
+        # for i, data in tqdm(enumerate(val_loader), total=int(len(val_loader.dataset) / val_loader.batch_size)):
+        for i, data in enumerate(val_loader):
+            local_batch, local_labels = data[0], data[1]
+            # local_batch['text'] = local_batch['text'].to(device)
+            # local_batch['image'] = local_batch['image'].to(device)
+            text = local_batch['text']
+            image = local_batch['image']
+            input_ids = text['input_ids']
+            attention_mask = text['attention_mask']
+            device_input = {
+                'image': image.to(device),
+                'text' : {
+                    'input_ids': input_ids.to(device),
+                    'attention_mask' : attention_mask.to(device)
+                }
+            }
+            device_local_labels = local_labels.to(device)
+            outputs = my_model(device_input)
+            loss = loss_func(outputs, device_local_labels)
 
-    X_panda = pd.DataFrame(X_orig)
-    Y_panda = pd.DataFrame(Y_orig)
+            val_running_loss += loss.item()
+            _, preds = torch.max(outputs.data, 1)
+            val_running_correct += (preds == device_local_labels).sum().item()
 
-    X = X_panda.values.tolist()
-    Y = (np.array(Y_panda.values.reshape(-1).tolist()) == 'true') * 1
+        val_loss = val_running_loss
+        val_accuracy = 100. * val_running_correct / len(val_loader.dataset)
 
-    sentences = [row[0] for row in X]
+        print(f'Val Loss: {val_loss:.4f}, Val Acc: {val_accuracy:.2f}')
+#         wandb.log({'val_accuracy': val_accuracy})
+#         wandb.log({'val_loss': val_loss})
 
-    # Embed text using BERT model.
-    text_tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
 
-    # model = DistilBertModel.from_pretrained('distilbert-base-uncased')
-    # {'input_ids': [[]], 'attention_mask': [[]]}
-    # inputs = text_tokenizer(sentences, return_tensors="pt", padding=True)
-    # text_tokenizer.convert_ids_to_tokens(inputs['input_ids'][0])  # to get back the tokens.
-    # outputs = model(**inputs)
-    # return
-
-    image_preprocessor = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-    train_dataset = ICErrorDataSet(X, Y, text_tokenizer, image_preprocessor)
-    train_dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True)
-
-    my_model = CaptionErrorDetectorBase()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(my_model.parameters())
-
-    max_epochs = 1
-    for epoch in range(max_epochs):
-        # Training the model.
-        my_model.train()
-        for local_batch, local_labels in train_dataloader:
-            # print(local_batch, local_labels)
-            optimizer.zero_grad()
-
-            outputs = my_model(local_batch)
-            loss = criterion(outputs, local_labels)
-            loss.backward()
-            optimizer.step()
-
-            print('loss={:.3g}'.format(loss))
-    print('done')
+# def test_load_data():
+#     X_orig = [
+#         ("person in read riding a motorcycle", "http://farm9.staticflickr.com/8186/8119368305_4e622c8349_z.jpg", "motorcycle.jpg"),
+#         ("lady cutting cheese with reversed knife", "http://farm1.staticflickr.com/1/127244861_ab0c0381e7_z.jpg", "cheese.jpg"),
+#         ("girl touching a buffalo", "http://farm3.staticflickr.com/2169/2118578392_1193aa04a0_z.jpg", "boy.jpg")
+#     ]
+#     Y_orig = ['true', 'true', 'false']
+#
+#     X_panda = pd.DataFrame(X_orig)
+#     Y_panda = pd.DataFrame(Y_orig)
+#
+#     X = X_panda.values.tolist()
+#     Y = (np.array(Y_panda.values.reshape(-1).tolist()) == 'true') * 1
+#
+#     sentences = [row[0] for row in X]
+#
+#     # Embed text using BERT model.
+#     text_tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+#
+#     # model = DistilBertModel.from_pretrained('distilbert-base-uncased')
+#     # {'input_ids': [[]], 'attention_mask': [[]]}
+#     # inputs = text_tokenizer(sentences, return_tensors="pt", padding=True)
+#     # text_tokenizer.convert_ids_to_tokens(inputs['input_ids'][0])  # to get back the tokens.
+#     # outputs = model(**inputs)
+#     # return
+#
+#     image_preprocessor = transforms.Compose([
+#         transforms.Resize(256),
+#         transforms.CenterCrop(224),
+#         transforms.ToTensor(),
+#         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+#     ])
+#     train_dataset = ICErrorDataSet(X, Y, text_tokenizer, image_preprocessor)
+#     train_dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+#
+#     my_model = CaptionErrorDetectorBase()
+#     criterion = nn.CrossEntropyLoss()
+#     optimizer = optim.Adam(my_model.parameters(),
+#             lr = 5e-5, # args.learning_rate - default is 5e-5
+#             eps = 1e-5
+#         )
+#     max_epochs = 2
+#     for epoch in range(max_epochs):
+#         # Training the model.
+#         my_model.train()
+#         for local_batch, local_labels in train_dataloader:
+#             # print(local_batch, local_labels)
+#             optimizer.zero_grad()
+#
+#             outputs = my_model(local_batch)
+#             loss = criterion(outputs, local_labels)
+#             loss.backward()
+#             optimizer.step()
+#
+#             print('loss={:.3g}'.format(loss))
+#     print('done')
 
 
 def test_image_model():
@@ -275,7 +338,7 @@ if __name__ == '__main__':
     # load_data()
     # test_image_model()
     # load_train_val_data()
-    train_model()
+    train_model(num_examples=10, batch_size=10)
     sys.exit(0)
 
 """
